03487b3a1ca09b12ae6a0b16a6aa81848f84bcc1
Ophelia-Boamah <ophybee8@gmail.com>
2020-12-20 23:27:36 +0000

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,91 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className='p-16 border-l-10 border-gray-300 h-screen '>
+      <nav className='flex items-center'>
+        <h2 className='text-brand-brown font-black text-lg'>
+          Peng <br />
+          Models
+        </h2>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <ul className='flex items-center ml-32 uppercase text-brand-brown'>
+          <li className='pr-16'>men</li>
+          <li className='pr-16'>women</li>
+          <li className='pr-16'>children</li>
+          <li className='pr-16'>contact</li>
+          <li className='pr-16'>about us</li>
+        </ul>
+      </nav>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className='flex'>
+        <div className='w-2/6 text-brand-brown'>
+          <h1 className=' font-black text-8xl'>Black &amp; Beautiful</h1>
+          <p className='mt-4'>
+            do you need amazing melanin models for you photoshoots modelling
+            events or outing, you just got them waiting right for you.
+          </p>
+          <div className='border-l-2 border-brand-brown h-24 my-6'></div>
+          <div className='flex'>
+            <div className='mr-10 -ml-2'>
+              <div className='text-brand-brown'>
+                <div>
+                  <FaFacebookF />
+                </div>
+                <div className='py-4'>
+                  <FaInstagram />
+                </div>
+                <div>
+                  <FaTwitter />
+                </div>
+              </div>
+            </div>
+            <div className='-mt-8 relative'>
+              <img
+                className='h-64 relative z-10'
+                src='/joyce/bottom.png'
+                alt=''
+              />
+              <div className='w-56 h-96 bg-gray-100 absolute -top-20 left-16'></div>
+            </div>
+          </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <div className='w-1/6'>
+          <div className='bg-gray-200'>
+            <img className='h-100 w-98' src='/joyce/joyce.png' alt='' />
+          </div>
+        </div>
+        <div className='w-3/6'>
+          <div className='bg-gray-200 w-2/3 ml-52 h-full'>
+            <div className='px-12 py-14'>
+              <h1 className='text-brand-brown text-opacity-30 text-4xl font-bold'>
+                Profile
+              </h1>
+              <div className='border-b border-brand-brown border-opacity-40 text-brand-brown '>
+                <p>Igna</p>
+                <h2 className='text-2xl -mt-2'>Joyce</h2>
+              </div>
+              <div className='flex border-b border-brand-brown border-opacity-40 text-brand-brown py-4'>
+                <div>
+                  <p className='text-sm'>Height</p>
+                  <div className='flex items-center'>
+                    <p>
+                      6 <span className='mt-2'>ft</span>
+                    </p>
+                    <p className='ml-2'>
+                      1 <span className='mt-2'>in</span>
+                    </p>
+                  </div>
+                </div>
+                <div className='ml-36'>
+                  <p className='text-sm'>Weight</p>
+                  <p>80</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          brown: '#7C5649',
+        },
+      },
+      borderWidth: {
+        10: '50px',
+      },
+      spacing: {
+        100: '700px',
+        98: '30rem',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
